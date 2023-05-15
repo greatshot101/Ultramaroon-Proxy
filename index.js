38a2f75210fe3bdacfedf547f68b36ef4fd5956f
greatshot101 <116977958+greatshot101@users.noreply.github.com>
2023-05-15 13:08:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,21 @@
-import createServer from '@tomphttp/bare-server-node';
+import Server from 'bare-server-node';
 import http from 'http';
-import express from 'express';
-import path from 'path';
-import * as url from 'url';
+import nodeStatic from 'node-static';
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const bare = createServer('/bare/');
-const app = express().use(express.static(path.join(__dirname, '/static')));
+
+const bare =  new Server('/bare/', '');
+const serve = new nodeStatic.Server('static/');
+
 const server = http.createServer();
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-app.get('/', (req, res) => {
-  res.sendFile('/static/index.html', (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error sending file');
-    }
-  });
-});
-
-server.on('request', (req, res) => {
-  if (bare.shouldRoute(req)) {
-    try {
-      bare.routeRequest(req, res);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error routing request');
-    }
-  } else {
-    try {
-      app(req, res);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error processing request');
-    }
-  }
+server.on('request', (request, response) => {
+    if (bare.route_request(request, response)) return true;
+    serve.serve(request, response);
 });
 
 server.on('upgrade', (req, socket, head) => {
-  if (bare.shouldRoute(req, socket, head)) {
-    try {
-      bare.routeUpgrade(req, socket, head);
-    } catch (err) {
-      console.error(err);
-      socket.end();
-    }
-  } else {
-    socket.end();
-  }
+	if(bare.route_upgrade(req, socket, head))return;
+	socket.end();
 });
 
-server.on('error', (err) => {
-  console.error('Server error:', err);
-});
-
-server.listen(process.env.PORT || 9090, () => {
-  console.log('Server is running on port', server.address().port);
-});
+server.listen(process.env.PORT || 8080);
