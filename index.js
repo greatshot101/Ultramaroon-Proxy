2d655015c6afe5b712a7bf11015a99b147c27aec
greatshot101 <116977958+greatshot101@users.noreply.github.com>
2023-05-15 13:02:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,64 @@
 import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
-import express from 'express'
-import path from 'path'
+import express from 'express';
+import path from 'path';
 import * as url from 'url';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const bare =  createServer('/bare/');
-const app = express().use(express.static(path.join(__dirname,'/static')))
+const bare = createServer('/bare/');
+const app = express().use(express.static(path.join(__dirname, '/static')));
 const server = http.createServer();
 
 app.use((err, req, res, next) => {
-	console.error(err.stack)
-	res.status(500).send('Something broke!')
-})
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
-app.get('/', (req,res) => {
-	res.sendFile('/static/index.html')
-})
+app.get('/', (req, res) => {
+  res.sendFile('/static/index.html', (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error sending file');
+    }
+  });
+});
 
 server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) {
-		bare.routeRequest(req, res);
-	} else {
-		try{
-			app(req,res)
-		}catch( err ){
-			console.error(err)
-			console.error("File was not found")
-		}
-	}
+  if (bare.shouldRoute(req)) {
+    try {
+      bare.routeRequest(req, res);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error routing request');
+    }
+  } else {
+    try {
+      app(req, res);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error processing request');
+    }
+  }
 });
 
 server.on('upgrade', (req, socket, head) => {
-	if (bare.shouldRoute(req, socket, head)) {
-		bare.routeUpgrade(req, socket, head);
-	}else{
-		socket.end();
-	}
+  if (bare.shouldRoute(req, socket, head)) {
+    try {
+      bare.routeUpgrade(req, socket, head);
+    } catch (err) {
+      console.error(err);
+      socket.end();
+    }
+  } else {
+    socket.end();
+  }
 });
 
-server.listen({
-	port: process.env.PORT || 9090,
+server.on('error', (err) => {
+  console.error('Server error:', err);
 });
 
+server.listen(process.env.PORT || 9090, () => {
+  console.log('Server is running on port', server.address().port);
+});
